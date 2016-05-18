065043a995626c622c5c9cbce45992e95b29f1a5
electrikjesus <electrikjesus@gmail.com>
2016-05-18 11:48:26 -0700

    Setup Bliss tags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,3 +54,19 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
 -include vendor/lge/d851/BoardConfigVendor.mk
+
+# BlissPop Config Flags
+BLISSIFY := true
+BLISS_O3 := true
+BLISS_STRICT := true
+BLISS_GRAPHITE := true
+BLISS_KRAIT := true
+Bliss_Build_Block :=1
+TARGET_TC_ROM :=4.9-linaro
+TARGET_TC_KERNEL :=4.9-linaro
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+
+
+# inherit some proprietary Bliss stuffs
+-include vendor/bliss/config/sm.mk
